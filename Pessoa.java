2a26d532a17cd89f4bb6a9124a518b32686e60c0
Nyllay <Nyllay>
2025-09-08 21:45:54 -0400

    Alguma coisa

--- Pessoa.java
+++ Pessoa.java
@@ -6,21 +6,54 @@ public class Pessoa {
     private String cpf;
     
     public String getNome(){
-        System.out.println("Seu nome é");
+        System.out.println("\nSeu nome é \n" + this.nome);
         return this.nome;
     }
 
     public int getIdade(){
-        System.out.println("e a sua idade é");
+        System.out.println("e a sua idade é \n" +this.idade );
         return this.idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
     }
     public void setCpf(String cpf){
         this.cpf = cpf;
     
     }
-    public String getCpf(){
-        System.out.println("e o seu CPF é esse");
-        return this.cpf;
+
+    public void setNome(String nome){
+        this.nome = nome;
     }
 
+    public String getCpf(){
+        System.out.println("e o seu CPF é esse \n" +this.cpf);
+        return this.cpf;
+    }
+    public void Andar(){
+        System.out.println("\nÓ "+nome+" começa a andar" );
+        try {
+            for (int i = 0; i < 5; i++) {
+                Thread.sleep(1500);
+                System.out.print("tac ");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+
+    }
+    public void Falar(){
+        System.out.println("e em seguida o "+nome+" começa a  falar");
+
+        try {
+            String fala = "Eu quero dormir";
+            for (char letra : fala.toCharArray()) {
+                System.out.print(letra);
+                Thread.sleep(300);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
