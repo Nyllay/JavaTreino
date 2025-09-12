8693df1c40a507d1160714e3b6fc89e8af3ce1a2
nyllay <nyllay>
2025-09-11 21:36:31 -0400

    saivi

--- demo/src/main/java/senac/com/rr/Main.java
+++ demo/src/main/java/senac/com/rr/Main.java
@@ -15,8 +15,48 @@ public class Main {
         System.out.println("Digite o seu email");
         String email = scanner.nextLine();
 
-        Funcionario F1 = new Funcionario(null, 0, null, null, null, null, null, null, null, 0, null, null)
+        System.out.println("Digite o seu telefone");
+        String telefone = scanner.nextLine();
+
+        System.out.println("Digite o seu endereço");
+        String endereco = scanner.nextLine();
+
+        System.out.println("Digite a data do seu nascimento");
+        String nascimento = scanner.nextLine();
+
+        System.out.println("Digite o seu cpf");
+        String cpf = scanner.nextLine();
+
+        System.out.println("Qual é a cor dos seus olhos");
+        String corDoOlho = scanner.nextLine();
+
+        System.out.println("Digite a sua matricula");
+        String matricula = scanner.nextLine();
+
+        System.out.println("Digite o seu salario");
+        double salario = scanner.nextDouble();
+
+         System.out.println("Digite o seu cargo");
+        String cargo = scanner.nextLine();
+
+         System.out.println("Digite o seu setor");
+        String setor = scanner.nextLine();
+
+        Funcionario F1 = new Funcionario(
+            nome, 
+            idade, 
+            email, 
+            telefone, 
+            endereco, 
+            nascimento, 
+            cpf, 
+            corDoOlho, 
+            matricula, 
+            salario,
+            cargo, 
+            setor);
 
         }
+
           
  }
